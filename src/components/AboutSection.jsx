3e4d4233ade811eb49552b192dfb8a691af58e82
Zakariah Patel <zakpatel04@gmail.com>
2025-10-14 21:15:49 -0400

    Bug fixes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
                             Get In Touch
                         </a>
                         <a 
-                            href="../public/Zakariah Patel_Resume.pdf" 
+                            href="/Zakariah-Patel_Resume.pdf" 
                             className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primar/20 transition-colors duration-300"
                             target="_blank"
                             rel="noopener noreferrer"
